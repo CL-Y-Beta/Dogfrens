9f5c9c5efc02e46c1364be8e7778a392525d81f6
benjamin koh <github@thekohs.net>
2023-08-26 11:09:20 +0800

    completed the listing framework all 7 routes

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,2 +1,3 @@
 class Listing < ApplicationRecord
+  belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "listings#home"
+  resources :listings
 end
