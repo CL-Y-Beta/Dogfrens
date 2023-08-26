b83c72cec80531361f17c35a5c78ea5499d75013
Clarissa Yeo <clarissayeo94@gmail.com>
2023-08-26 13:55:44 +0800

    Added functions into bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,40 @@
 class BookingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @bookings = Booking.where(id: current_user.id)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path, notice: "Booking deleted."
+  end
+
+  def booking_params
+    params.require(:booking).permit(
+      :listing_id,
+      :service_provider_id,
+      :dog_owner_id,
+      :confirmed,
+      :booking_price
+    )
+  end
 end
