d8929d114ee431fbceb375f6bfd0edaf2a76ad04
Hon Aloysius <aloysiushon@hotmail.com>
2023-09-02 11:25:13 +0800

    add category to listing table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_124058) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_02_032221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -85,6 +85,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_124058) do
     t.integer "discount", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "category"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
